8b33f186874ec30f72a05d102cb33b1750f9d6da
NRS08 <nirantravindra20bcs138@iiitkottayam.ac.in>
2023-06-20 02:56:24 +0530

    global context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './CSS/Navbar.css';
 import { Button } from '@chakra-ui/react';
 import WalletIcon from '@mui/icons-material/Wallet';
+import { useGlobalContext } from '../context';
 
 const Navbar = () => {
+  const { account, setAccount, contract, setContract, provider, setProvider } =
+    useGlobalContext();
+
   return (
     <div className="Nav-body">
       <div className="logo">Supply Chain Management</div>
---
+++ src/context.js
@@ -0,0 +1,26 @@
+import React, { useState, useContext } from 'react';
+const AppContext = React.createContext();
+const AppProvider = ({ children }) => {
+  const [account, setAccount] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [provider, setProvider] = useState(null);
+  return (
+    <AppContext.Provider
+      value={{
+        account,
+        setAccount,
+        contract,
+        setContract,
+        provider,
+        setProvider,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider };
