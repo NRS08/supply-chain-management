13523624663ab409d954f85dca2a331e03055296
NRS08 <nirantravindra20bcs138@iiitkottayam.ac.in>
2023-06-20 15:58:11 +0530

    solidity code added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,24 @@ import React from 'react';
 import './CSS/Home.css';
 import { Button, Input } from '@chakra-ui/react';
 import SearchIcon from '@mui/icons-material/Search';
+import { useGlobalContext } from '../context';
 
 const Home = () => {
+  const { account, setAccount, contract, setContract, provider, setProvider } =
+    useGlobalContext();
+  async function fetch() {
+    const tx = await contract.assignProduct('Wheat', 1000);
+    await tx.wait();
+    const tx2 = await contract.sellProduct(
+      1000,
+      '0xa777122Ad242e2800781e8692a0591b964396aF4',
+      1000000,
+      '21.0010° N, 78.2307° E'
+    );
+    await tx2.wait();
+    const data = await contract.getProductDetails(1000);
+    console.log(data);
+  }
   return (
     <div className="Home-body">
       <div className="content">
@@ -17,7 +33,7 @@ const Home = () => {
         <p className="enter">Enter the product Id to check its information</p>
         <div className="search">
           <Input placeholder="Enter Product ID" />
-          <Button colorScheme="teal" variant="solid">
+          <Button onClick={fetch} colorScheme="teal" variant="solid">
             <SearchIcon />
           </Button>
         </div>
