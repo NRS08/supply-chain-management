7a494441a9e5c16bbc89166046e26f718e0533f0
Nirant R. Sontakke <78961551+NRS08@users.noreply.github.com>
2023-06-20 15:51:05 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,55 @@
-import React from 'react';
-import './CSS/Navbar.css';
-import { Button } from '@chakra-ui/react';
-import WalletIcon from '@mui/icons-material/Wallet';
-import { useGlobalContext } from '../context';
+import React from "react";
+import "./CSS/Navbar.css";
+import { Button } from "@chakra-ui/react";
+import WalletIcon from "@mui/icons-material/Wallet";
+import { ethers } from "ethers";
+import SCM from "../artifacts/contracts/SupplyChain.sol/SupplyChain.json";
+import { useGlobalContext } from "../context";
 
 const Navbar = () => {
   const { account, setAccount, contract, setContract, provider, setProvider } =
     useGlobalContext();
+  async function ButtonClick() {
+    console.log("window.ethereum:", window.ethereum);
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log("provider:", provider);
+      const loadProvider = async () => {
+        window.ethereum.on("chainChanged", () => {
+          window.location.reload();
+        });
 
+        window.ethereum.on("accountsChanged", () => {
+          window.location.reload();
+        });
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        let contractAddress = "0x3B117Ff72803eadECDc3944e414B5fb0931d872C";
+        const contract = new ethers.Contract(contractAddress, SCM.abi, signer);
+
+        setContract(contract);
+        setProvider(provider);
+        console.log(typeof contract);
+      };
+      provider && loadProvider();
+    } else {
+      alert("Install Metamask");
+    }
+  }
   return (
     <div className="Nav-body">
       <div className="logo">Supply Chain Management</div>
       <div className="connect">
-        <Button rightIcon={<WalletIcon />} colorScheme="green">
-          Connect
+        <Button
+          onClick={ButtonClick}
+          rightIcon={<WalletIcon />}
+          colorScheme="red"
+          variant="outline"
+        >
+          {account === null ? "Connect" : account}
         </Button>
-        {/* rightIcon={<WalletIcon />} */}
       </div>
     </div>
   );
