c5f67c88ed2be9339f2cdc598ce9722c957244cd
NRS08 <nirantravindra20bcs138@iiitkottayam.ac.in>
2023-07-03 00:09:12 +0530

    changes

--- src/components/Items.js
+++ src/components/Items.js
@@ -18,6 +18,7 @@ import { useNavigate } from "react-router-dom";
 import SearchIcon from "@mui/icons-material/Search";
 import Loader from "./Loader";
 import axios from "axios";
+import "./CSS/Items.css";
 
 export default function Items() {
   const url =
@@ -95,79 +96,68 @@ export default function Items() {
         </Heading>
       ) : (
         <Box
-          width={"100%"}
           display={"flex"}
-          justifyContent={{
-            base: "center",
-            sm: "center",
-            md: "space-around",
-            lg: "center",
-          }}
-          alignItems={"center"}
-          gap={4}
-          p={{ base: 2, md: 6, lg: 8 }}
-          flexWrap={"wrap"}
-          textTransform={"capitalize"}
+          justifyContent={"center"}
+          width={"100%"}
+          p={0}
+          m={0}
+          // bg={"red"}
         >
-          {data.map((item) => {
-            return (
-              <Box
-                key={item._id}
-                py={2}
-                flex={{
-                  base: "0 0 60%",
-                  sm: "0 0 50%",
-                  md: "0 0 33.33%",
-                  lg: "0 0 20%",
-                }}
-                //   flex={"0 0 20%"}
-              >
-                <Box
-                  maxW={"320px"}
-                  w={"full"}
-                  bg={"gray.900"}
-                  boxShadow={"2xl"}
-                  rounded={"lg"}
-                  p={6}
-                  textAlign={"center"}
-                >
-                  <Heading fontSize={"2xl"} fontFamily={"body"}>
-                    {`ID - ${item.prodID}`}
-                  </Heading>
-                  <Heading fontSize={"2xl"} fontFamily={"body"}>
-                    {`Item - ${item.Iname}`}
-                  </Heading>
-                  <Text fontWeight={600} color={"gray.500"} mb={2}>
-                    {`${item.amount} Kg`}
-                  </Text>
-                  <Heading fontSize={"xl"} fontFamily={"body"}>
-                    {`Seller - ${item.name}`}
-                  </Heading>
-                  <Text fontWeight={600} color={"gray.500"} mb={4}>
-                    {item.role}
-                  </Text>
-                  <Box textAlign={"center"} color={"gray.400"} px={3}>
-                    <Text fontWeight={"600"} color={"white"}>
-                      Harvested on{" "}
+          <Box
+            className="items"
+            p={{ base: 2, md: 6, lg: 8 }}
+            textTransform={"capitalize"}
+          >
+            {data.map((item) => {
+              return (
+                <Box key={item._id} py={2}>
+                  <Box
+                    maxW={"320px"}
+                    w={"full"}
+                    bg={"gray.900"}
+                    boxShadow={"2xl"}
+                    rounded={"lg"}
+                    p={6}
+                    textAlign={"center"}
+                  >
+                    <Heading fontSize={"2xl"} fontFamily={"body"}>
+                      {`ID - ${item.prodID}`}
+                    </Heading>
+                    <Heading fontSize={"2xl"} fontFamily={"body"}>
+                      {`Item - ${item.Iname}`}
+                    </Heading>
+                    <Text fontWeight={600} color={"gray.500"} mb={2}>
+                      {`${item.amount} Kg`}
                     </Text>
-                    <Text color={"#e95065"}>{item.harvestDate}</Text>
-                  </Box>
+                    <Heading fontSize={"xl"} fontFamily={"body"}>
+                      {`Seller - ${item.name}`}
+                    </Heading>
+                    <Text fontWeight={600} color={"gray.500"} mb={4}>
+                      {item.role}
+                    </Text>
+                    <Box textAlign={"center"} color={"gray.400"} px={3}>
+                      <Text fontWeight={"600"} color={"white"}>
+                        Harvested on{" "}
+                      </Text>
+                      <Text color={"#e95065"}>{item.harvestDate}</Text>
+                    </Box>
 
-                  <Stack mt={8} direction={"row"} spacing={4}>
-                    <Button
-                      flex={1}
-                      variant={"outline"}
-                      colorScheme="green"
-                      fontSize={"sm"}
-                      rounded={"full"}
-                    >
-                      {`Contact - ${item.contact}`}
-                    </Button>
-                  </Stack>
+                    <Stack mt={8} direction={"row"} spacing={4}>
+                      <Button
+                        flex={1}
+                        variant={"outline"}
+                        colorScheme="green"
+                        fontSize={"sm"}
+                        rounded={"full"}
+                      >
+                        {`Contact - ${item.contact}`}
+                      </Button>
+                    </Stack>
+                  </Box>
                 </Box>
-              </Box>
-            );
-          })}
+              );
+            })}
+          </Box>
         </Box>
       )}
     </>
