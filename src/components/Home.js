d530e1db47f985fc263d7b1dda8fffc764e00113
NRS08 <nirantravindra20bcs138@iiitkottayam.ac.in>
2023-07-04 17:12:25 +0530

    item fetch frontend

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import "./CSS/Home.css";
-import { Button, Input } from "@chakra-ui/react";
+import {
+  Button,
+  Input,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
 import SearchIcon from "@mui/icons-material/Search";
 import Navbar from "./Navbar";
 import { useNavigate } from "react-router-dom";
@@ -9,6 +21,10 @@ import { ethers } from "ethers";
 import SCM from "../artifacts/contracts/SupplyChain.sol/SupplyChain.json";
 
 const Home = () => {
+  let INDRupees = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "INR",
+  });
   const navigate = useNavigate();
   const [id, setid] = useState(null);
 
@@ -18,6 +34,13 @@ const Home = () => {
       navigate("/user");
     }
   }, []);
+
+  const prices = [1000, 99999, 1000199];
+  const locations = [
+    "Arvi, Wardha, Maharashtra, 442200, India",
+    "Mumbai",
+    "Delhi",
+  ];
 
   const provider = new ethers.providers.JsonRpcProvider(
     "https://eth-sepolia.g.alchemy.com/v2/-4e5fsDjluNgzUNf9u0nNElwVvNV2QYq"
@@ -37,6 +60,8 @@ const Home = () => {
   }
 
   async function fetch() {
+    const table = document.querySelector(".itemInfo");
+    table.style.display = "none";
     try {
       const product = await contract.getProductDetails(id);
       console.log(
@@ -48,6 +73,7 @@ const Home = () => {
         Number(product.productTimestamp),
         product.productOwner
       );
+      table.style.display = "flex";
     } catch (error) {
       if (error.message.includes("Invalid product ID")) {
         alert("Product ID doesn't exist");
@@ -76,6 +102,34 @@ const Home = () => {
               <SearchIcon />
             </Button>
           </div>
+        </div>
+        <div className="itemInfo">
+          <TableContainer
+            w={"auto"}
+            border={"1px solid grey"}
+            borderRadius={"1rem"}
+            p={4}
+          >
+            <Table variant="simple">
+              <TableCaption>Imperial to metric conversion factors</TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>Prices</Th>
+                  <Th>Location</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {prices.map((price, index) => {
+                  return (
+                    <Tr>
+                      <Td>{INDRupees.format(price)}</Td>
+                      <Td>{locations[index]}</Td>
+                    </Tr>
+                  );
+                })}
+              </Tbody>
+            </Table>
+          </TableContainer>
         </div>
       </div>
     </>
